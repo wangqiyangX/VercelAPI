94e7deff8909cc60014f1044b3e3d8ccce5ff081
qiyang <wangqiyangx@gmail.com>
2025-11-26 15:57:14 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VercelAPI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "VercelAPI",
+            targets: ["VercelAPI"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "VercelAPI"
+        ),
+        .testTarget(
+            name: "VercelAPITests",
+            dependencies: ["VercelAPI"]
+        ),
+    ]
+)
